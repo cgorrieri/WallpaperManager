782d8acb57411b88db80be918ab7b4823f7e88cc
Cyril <cyril@ubuntu.ubuntu-domain>
2011-03-31 20:01:33 +0200

    Ajout d'un refresh de l'activité courrante

--- src/com/wallpapers_manager/cyril/WallpapersTabActivityGroup.java
+++ src/com/wallpapers_manager/cyril/WallpapersTabActivityGroup.java
@@ -1,10 +1,13 @@
 package com.wallpapers_manager.cyril;
 
-import com.wallpapers_manager.cyril.wallpapers.FoldersActivity;
-import com.wallpapers_manager.cyril.wallpapers.WallpapersActivity;
-
+import android.app.Activity;
+import android.app.LocalActivityManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+
+import com.wallpapers_manager.cyril.wallpapers.FoldersActivity;
+import com.wallpapers_manager.cyril.wallpapers.WallpapersActivity;
 
 public class WallpapersTabActivityGroup extends TabActivityGroup {
 	
@@ -19,6 +22,16 @@ public class WallpapersTabActivityGroup extends TabActivityGroup {
 		this.startChildActivity("FoldersActivity", new Intent(this,
 			FoldersActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 
+	}
+	
+	public void refreshCurrent() {
+		Intent lastIntent = new Intent(this, getCurrentActivity().getClass());
+		
+		LocalActivityManager localActivityManager = getLocalActivityManager();
+		String lastId = mIdList.get(mIdList.size() - 1);
+
+		localActivityManager.destroyActivity(lastId, true);
+		startChildActivity(lastId, lastIntent);
 	}
 	
 	
